c0ba2fb7d9f1973e398b5f48d0d7674338dcbdc8
Ubuntu <remiadmin@Remi-Dev.rcxwjamrkjzexfsueegwnuwgve.yx.internal.cloudapp.net>
2018-03-14 13:09:47 +0000

    First commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'lendflo-arelle',
+  packages = ['lendflo-arelle'], # this must be the same as the name above
+  version = '0.1',
+  description = 'arelle xblr modified by Lendflo',
+  author = 'Remi Tuyaerts / Przemyslaw Winszczyk',
+  author_email = 'remi.tuyaerts@lendflo.com',
+  url = 'https://github.com/LendFlo/pip-arelle', # use the URL to the github repo
+  download_url = 'https://github.com/LendFlo/pip-arelle/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['arelle', 'lendflo', 'fork'], # arbitrary keywords
+  classifiers = [],
+)
