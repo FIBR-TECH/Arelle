ca41ee3a7325f3723e8df7dbcc39804c5e72bd8b
przemyslaw.winszczyk@lendflo/com <przemekadmin@Przemek-Dev.rcxwjamrkjzexfsueegwnuwgve.yx.internal.cloudapp.net>
2018-03-21 16:18:08 +0000

    New version with comments in Ctrl

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Remi Tuyaerts / Przemyslaw Winszczyk',
   author_email = 'remi.tuyaerts@lendflo.com',
   url = 'https://github.com/LendFlo/pip-arelle', # use the URL to the github repo
-  download_url = 'https://github.com/LendFlo/pip-arelle/archive/0.2.1.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/LendFlo/pip-arelle/archive/0.2.2.tar.gz', # I'll explain this in a second
   keywords = ['arelle', 'lendflo', 'fork'], # arbitrary keywords
   classifiers = [],
 )
